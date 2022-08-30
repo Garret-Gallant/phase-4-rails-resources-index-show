187d79c29ad22feb39d930fd825d3fc47f1e52ca
Garret Gallant <gallanttechnological@gmail.com>
2022-08-30 11:38:00 -0600

    commit tree 98b98f6fd8f31bbc7fd1261c5c69f472ae5a0828

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -14,4 +14,9 @@ class BirdsController < ApplicationController
     end
   end
 
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: created
+  end
+
 end
